b7363e6f7ebcc39c9042f21f40a528713211da54
brendan-h <bconor.h@gmail.com>
2021-04-11 12:36:57 -0500

    Added sphinx documentation files

---
+++ docs/source/conf.py
@@ -0,0 +1,60 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../hypro'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'hypro'
+copyright = '2021, Nanfeng Liu'
+author = 'Nanfeng Liu'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add napoleon to the extensions list
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
+    'recommonmark'
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+napoleon_numpy_docstring = True
+napoleon_google_docstring = False
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,38 @@
+HyPro Documentation v1.0.0
+==========================
+
+.. automodule:: hypro
+    :members:
+
+.. toctree::
+    :maxdepth: 2
+    :caption: Contents:
+
+    ../../README.md
+    HyspexPro
+    AtmCorr
+    AtmLUT
+    Boresight
+    Classification
+    CreateSCA
+    DEM
+    ENVI
+    Figure
+    GeoReferencing
+    GeoRectification
+    Geography
+    ImageMerging
+    Radiometry
+    Spectra
+    SmileEffect
+    SensorModel
+    WVC
+    VIS
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
